e1dffca50b17455e5bf6df2faa8bac8094c7fed9
sinaneker <selsyourself@gmail.com>
2014-01-03 16:11:00 +0100

    testing wheter th autoloader for the interfaces works

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php ?>
+<?php 
+require_once __DIR__ . '/vendor/autoload.php'; 
+
+echo "hi";
+
+class Bla implements \Core\Interfacs\Main
+{
+	function __construct()
+	{
+		echo "foo";
+	}	
+}
+$b = new Bla();
