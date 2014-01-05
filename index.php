41e6da9b177daafa8a7999d6ceaf91e172d60a81
sinaneker <selsyourself@gmail.com>
2014-01-05 16:00:58 +0100

    testing

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php 
-require_once __DIR__ . '/vendor/autoload.php'; 
-
+require_once __DIR__ . '/vendor/autoload.php';
 echo "hi";
-
-class Bla implements \Core\Interfacs\Main
-{
-	function __construct()
-	{
-		echo "foo";
-	}	
-}
-$b = new Bla();
