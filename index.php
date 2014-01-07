8a5d884ef4451430a4027f5a112813271a48cc20
sinaneker <selsyourself@gmail.com>
2014-01-07 21:29:53 +0100

    added documentation

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php 
 require_once __DIR__ . '/vendor/autoload.php';
+use \Core\View;
+use \Core\Controller;
 
-
-$klein = new \Klein\Klein();
+$controller = new Controller(new \Klein\Klein(), []);
+$view = new View($controller);
+$view->hold("GET", "/hi", function ()
+{
+	return "hi";
+})->dispatch();
--- src/Core/View.php
+++ src/Core/View.php
@@ -1,16 +1,50 @@
 <?php
 namespace Core;
+
+/*
+ * The View class.
+ * 
+ * @api
+ * 
+ * @package Core
+ * 
+ * */
 class View
 {
+	/*
+	 * @type string \Core\View::DEFAULT_RENDER_FILE Defines the default file that is rendered if no other is provided.
+	 * */
 	const DEFAULT_RENDER_FILE = "/Main.php";
 	
-    protected $controller;
+	/*
+	 * @type \Core\Controller|null Controller instance.
+	 * */
+    protected $controller = null;
     
-    public function __construct(\Core\Classes\Controller $controller)
+	/*
+	 * Initialize the View class and sets the \Core\Controller $controller instance.
+	 * 
+	 * @api
+	 * 
+	 * @param \Core\Controller $controller The controller instance.
+	 * 
+	 * @property-write \Core\Controller $controller
+	 * */
+    public function __construct(\Core\Controller $controller)
     {
         $this->controller = $controller;
     }
     
+	/*
+	 * Shows the default page to render.
+	 * 
+	 * @api
+	 * 
+	 * @param \Klein\ServiceProvider $service The service instance from Klein php.
+	 * 		  mixed[] $args
+	 * 
+	 * @return void
+	 * */
     public function showDefault(\Klein\ServiceProvider $service, array $args = [])
     {
         $options = $this->controller->getOptions("defaults");
@@ -21,6 +55,15 @@ class View
         $service->render(VIEWS_PATH.self::DEFAULT_RENDER_FILE, $args);
     }
     
+	/*
+	 * Holds a route and registers it to the Klein instance.
+	 * 
+	 * @api
+	 * 
+	 * @param string $method The request method POST|PUT|DELETE|GET.
+	 * 		  string $route The route that is ment.
+	 * 		  callable $function The function that will be fired if the route is invoked.  
+	 * */
     public function hold($method = "GET", $route = "/", callable $function = null)
     {
         if (!is_string($method))
@@ -42,6 +85,15 @@ class View
         return $this;
     }
     
+	/*
+	 * Dispatchs the router.
+	 * 
+	 * @api
+	 * 
+	 * @param void
+	 * 
+	 * @return \Core\View $this Returns the object instance.
+	 * */
     final public function dispatch()
     {
         $this->controller->getKlein()->dispatch();
